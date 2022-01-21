0e4bf2ba0d822394a8249d87ab3abb6e1f6d77f5
Arslan Jan Soomro <arslanjs.dev@gmail.com>
2022-01-21 02:00:52 -0800

    Add comment in Tail Config and Remove one Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
+
+  //purge:{ enabled: process.env.NODE_ENV === 'production', content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'], },
+
   theme: {
     extend: {
       fontFamily: {
